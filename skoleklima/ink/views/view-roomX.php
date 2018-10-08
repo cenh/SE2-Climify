34c07d9ea0d296b318335094f63afb3c07f8eb51
98christianp <christianpetersen98@gmail.com>
2018-10-08 11:16:24 +0200

    testing form with button

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -1,5 +1,9 @@
 
 <div class="single-view view-roomX">
+    <form id="temp">
+        Value:<br>
+        <input type="number" name="value">
+    </form>
     <button id="my_button">
         my button
     </button>
