771c46e9b40e3a0d3c013d6c8a3c323ca49b6f57
Christian Hansen <cralle@outlook.com>
2018-10-08 16:59:19 +0200

    Influx db (#1)
    * Test influx
    
    * Bypass API
    
    * bypass
    
    * More hardcoded queries

--- skoleklima/api/api-get-influx-info.php
+++ skoleklima/api/api-get-influx-info.php
@@ -3,31 +3,34 @@
 //************************************************
 //	Get influx database info
 //************************************************
-    
+
 require_once "../meta.php";
 
 if( $currentUserID == ""){
-    echo '{"status":"error"}';
+    echo '{"status":"No current user"}';
     exit;
-} 
+}
 
 // Validate API key
 $apiPassword = API_PASSWORD;
 $phase_api_key = clean($_POST['fAY2YfpdKvR']);
 
-if( $apiPassword !== $phase_api_key){
-    echo '{"status":"error"}';
-    exit;
-} 
+//if( $apiPassword !== $phase_api_key){
+//    echo '{"status":"Incorrect API Password"}';
+//    exit;
+//}
 
-$returnFromIfx = file_get_contents("http://193.200.45.37:8086/query?u=".$influxUser."&p=".$influxPass."&db=".$influxName."&q=SHOW%20MEASUREMENTS%20limit%201");
+$returnFromIfx = file_get_contents("http://localhost:8086/query?u=".$influxUser."&p=".$influxPass."&db=scadb&q=SELECT%20value%20FROM%20mem");
+echo $returnFromIfx;
 
 $returnFromIfx = json_decode($returnFromIfx, true);
 
+echo $returnFromIfx;
+
 if ($returnFromIfx[results][0][series][0][name] != "measurements") {
-    echo '{"status":"error"}';
+    echo '{"status":"Unable to retrieve from InfluxDB"}';
     exit;
-} 
+}
 
 echo '{"status":"ok"}';
 
