23c14c8dc1f95c94e67a2d101cc53ba8a8f67395
ciokos <wciokos@gmail.com>
2018-10-08 12:59:39 +0200

    layout roomx 1

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -1,14 +1,23 @@
+<style>
+    .view-roomX{
+        width: 600px;
+        height: 400px;
+        padding: 0;
+        margin: 0;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        margin-top: -200px;
+        margin-left: -300px
+    }
+</style>
 
 <div class="single-view view-roomX">
-    <section>
     <form id="temp">
         Value:<br>
         <input type="number" name="value">
     </form>
-    </section>
-    <section>
     <button id="my_button">
         my button
     </button>
-    </section>
 </div>
