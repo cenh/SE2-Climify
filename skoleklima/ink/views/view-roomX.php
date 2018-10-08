456d1836db03192a7bf09209750411909dbd41b2
ciokos <wciokos@gmail.com>
2018-10-08 13:21:35 +0200

    layout roomx 6

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -2,33 +2,18 @@
     .view-roomX{
         width: 100%;
         height: 100%;
-        padding: 0px;
-        margin: 0px;
+        padding: 0;
+        margin: 0;
         position: relative;
         background-color: #00caf2;
     }
 
     .roomx-layout{
-        margin: 0px;
+        margin: 0;
         position: absolute;
         width: 100%;
         height: 50%;
         border: #9933ff;
-    }
-
-    #temp{
-        margin: 0;
-        top: 50%;
-        left: 50%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-    #my_button{
-        margin: 0;
-        top: 50%;
-        left: 50%;
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
     }
 </style>
 
