deb17d1b0ef7550a3cf7a4f14f6a9b7c4c40e2d7
98christianp <christianpetersen98@gmail.com>
2018-10-08 14:38:24 +0200

    View ROomX updated

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -13,16 +13,18 @@
             <!-- TODO: -->
             <span><h4>The current temperature is: </h4></span>
         </div>
-        <div class="view-roomX-data-box" style="border:1px solid #92b2c7; padding:10px">
+        <div class="view-roomX-data-box" style="border:1px solid #92b2c7; padding:15px">
            <span>
                 <form id="temp" class="my-center">
                     Set temperature:
                     <input type="number" name="value">
                 </form>
-               <button id="my_button">
-                    my button
-                </button>
            </span>
+        </div>
+        <div class="button-float" style="float:right; padding:10px">
+            <button id="my_button">
+                Send request
+            </button>
         </div>
     </div>
 
