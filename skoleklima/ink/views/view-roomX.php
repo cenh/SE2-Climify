5485666c52a68d562967ceaa44edbb3595312880
ciokos <wciokos@gmail.com>
2018-10-08 13:44:37 +0200

    layout roomx 8

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -4,15 +4,23 @@
         width: 100%;
     }
     .wide-row{
-        height: 200px;
+        height: 400px;
         width: 100%;
         border-color: #00caf2;
+        position: relative;
+    }
+    .my-center{
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
 
 <div class="single-view view-roomX" ali>
     <div class="wide-row">
-        <form id="temp">
+        <form id="temp" class="my-center">
             Value:<br>
             <input type="number" name="value">
         </form>
