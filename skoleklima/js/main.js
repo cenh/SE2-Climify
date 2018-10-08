54d343ac3ca6245d7e2a026b0ee70503ca57cd6e
ciokos <wciokos@gmail.com>
2018-10-08 16:46:53 +0200

    add data to graph 2

--- skoleklima/js/main.js
+++ skoleklima/js/main.js
@@ -139,46 +139,6 @@ if (currentUserID == "") {
     requestAutoLoginSystem();
 };
 
-var temps = [12, 19, 3, 5, 2, 3];
-
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: temps,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
 // Login
 $(".login-input").click(function () {
     if ($(this).hasClass("wrong-login")) {
@@ -842,10 +802,8 @@ $("#my_button").click(function () {
         client.publish(message);
         console.log("published: "+message);
         document.getElementById('current_set_temp').innerHTML = text;
-        console.log(temps);
         temps.push(temperature);
         console.log(temps);
-
     }
 
 // called when the client loses its connection
@@ -862,4 +820,13 @@ $("#my_button").click(function () {
     }
 
 });
+
+var temps = [12, 19, 3, 5, 2, 3];
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: temps
+});
+
 
