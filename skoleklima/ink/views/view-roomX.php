1b9b9a75a09ac781856420e4c1e8eb3216ecf41f
ciokos <wciokos@gmail.com>
2018-10-08 12:53:09 +0200

    layout roomx

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -1,10 +1,14 @@
 
 <div class="single-view view-roomX">
+    <section>
     <form id="temp">
         Value:<br>
         <input type="number" name="value">
     </form>
+    </section>
+    <section>
     <button id="my_button">
         my button
     </button>
+    </section>
 </div>
