e82b433019c7f66e7ba87b82f771160ecdc8248c
98christianp <christianpetersen98@gmail.com>
2018-10-09 10:07:07 +0200

    Fixed momentJS cdn link

--- skoleklima/index.php
+++ skoleklima/index.php
@@ -62,7 +62,7 @@
 		}
 
 	?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/locale/da.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
