3bacaa282df76a34b7cda9b700663f3202ac94ed
98christianp <christianpetersen98@gmail.com>
2018-10-09 09:34:08 +0200

    Edited size of graph 2

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -9,8 +9,8 @@
     </div>
     <hr>
     <!-- TODO: view data -->
-  <div class="view-roomx-data-box" style="width:600px; height:600px;">
-      <canvas id="myChart" style="width:100%"></canvas>
+  <div class="view-roomx-data-box" style="width:600px; height:500px;">
+      <canvas id="myChart" style="width:100%; height: 100%;"></canvas>
   </div>
     <div class="view-roomX-data-box">
         <div class="temperature">
