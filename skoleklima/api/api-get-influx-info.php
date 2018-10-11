fdcf1ab4f8fdd6d54bd644b6ec0e03c576fa4bfa
98christianp <christianpetersen98@gmail.com>
2018-10-11 17:17:10 +0200

    php api call inside view-roomx

--- skoleklima/api/api-get-influx-info.php
+++ skoleklima/api/api-get-influx-info.php
@@ -15,14 +15,16 @@ if( $currentUserID == ""){
 $apiPassword = API_PASSWORD;
 $phase_api_key = clean($_POST['fAY2YfpdKvR']);
 
-if( $apiPassword !== $phase_api_key){
-    echo '{"status":"error"}';
-    exit;
-} 
+//if( $apiPassword !== $phase_api_key){
+//    echo '{"status":"error"}';
+//    exit;
+//}
 
 $returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readBattery");
 
 $returnFromIfx = json_decode($returnFromIfx, true);
+echo $returnFromIfx;
+
 
 if ($returnFromIfx[results][0][series][0][name] != "measurements") {
     echo '{"status":"error"}';
