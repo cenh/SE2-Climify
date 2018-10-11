7961c53808f9ebed8d4e8bcac88c9d1f9bbac839
Cralle <cralle@outlook.com>
2018-10-11 17:37:23 +0200

    back

--- skoleklima/api/api-get-sensor-info.php
+++ skoleklima/api/api-get-sensor-info.php
@@ -1,4 +1,5 @@
 <?php
 $returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readBattery");
+echo $returnFromIfx;
 return $returnFromIfx;
 ?>
