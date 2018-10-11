4f45787679f3077899e5d7f1caa715d6d47ab38e
98christianp <christianpetersen98@gmail.com>
2018-10-11 17:15:37 +0200

    php api call inside view-roomx

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -14,7 +14,7 @@
 <!--            REFRESHDATA-->
 <!--        </button>-->
 <!--    </div>-->
-    <
+
     <!-- TODO: view data -->
     <div class="data-display-box">
         <div class="temperature-graph-box">
@@ -58,9 +58,6 @@
             </button>
         </div>
     </div>
-    <?php
-        $response = file_get_contents('http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readBattery');
-        $response = json_decode($response);
-    ?>
+
 </div>
 
