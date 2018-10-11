4c83025fff27de383045a13f3ae55894f9705640
98christianp <christianpetersen98@gmail.com>
2018-10-11 19:14:36 +0200

    added humidity

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -7,7 +7,7 @@
     $temperature = file_get_contents(   "http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readOutdoorTemperature");
     $temperature = json_encode($temperature, true)
 
-
+    $humidity = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readHumidity")
 
     ?>
     <div class="roomX-top">
@@ -34,13 +34,13 @@
             <div class="view-roomX-data-box">
                 <span>
                     <i class="menu-link-ico nav-icon fa fa-thermometer-half" aria-hidden="true"></i>
-                    <h4 style="float:left; padding-top:10px; padding-right:20px" id="temp_cur"></h4>
+                    <h4 style="float:left; padding-top:10px; padding-right:60px" id="temp_cur"></h4>
                 </span>
             </div>
             <div class="view-roomX-data-box">
                 <span>
                     <i class="menu-link-ico nav-icon fas fa-tint"></i>
-                    <h4 style="float:left; padding-top:10px; padding-right:20px" id="humi_cur"></h4>
+                    <h4 style="float:left; padding-top:10px; padding-right:60px" id="humi_cur"></h4>
                 </span>
             </div>
             <div class="view-roomX-data-box">
@@ -49,7 +49,7 @@
             <div class="view-roomX-data-box">
                 <span>
                     <i class="menu-link-ico nav-icon fa fa-battery-half"></i>
-                    <h4 style="float:right; padding-top:10px; padding-right:20%" id="bat_lvl"></h4>
+                    <h4 style="float:right; padding-top:10px; padding-right:60%" id="bat_lvl"></h4>
                 </span>
             </div>
         </div>
@@ -86,10 +86,15 @@
     var json_batt = JSON.parse(obj);
     // get the latest battery reading
     var latest = json_batt.results[0].series[0].values.slice(-1)[0];
-    document.getElementById("bat_lvl").innerHTML = latest[1]+ "%";
+    document.getElementById("bat_lvl").innerHTML = latest[1].substring(0,6)+ "%";
     var t_obj = <?php echo $temperature; ?>;
     var json_t = JSON.parse(t_obj);
     var t_latest = json_t.results[0].series[0].values.slice(-1)[0];
     document.getElementById("temp_cur").innerHTML = t_latest[1];
+
+    var h_obj = <?pho echo $humidity; ?>;
+    var json_h = JSON.parse(h_obj);
+    var h_latest = json_h.results[0].series[0].values.slice(-1)[0];
+    document.getElementById("humi_cur").innerHTML = h_latest[1];
 
 </script>
