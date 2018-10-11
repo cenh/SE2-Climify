b2dab1515d9fc1fa1967c08be67e7e536f35cb5c
Cralle <cralle@outlook.com>
2018-10-11 17:33:32 +0200

    Simple first

--- skoleklima/api/api-get-sensor-info.php
+++ skoleklima/api/api-get-sensor-info.php
@@ -1,7 +1,4 @@
 <?php
-$sensor = $_GET['sensor'];
-echo "Sensor: " . $sensor . "\n";
-$returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20".$sensor);
-echo $returnFromIfx;
+$returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readBattery");
 return $returnFromIfx;
 ?>
