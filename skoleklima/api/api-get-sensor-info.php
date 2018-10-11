bcab2e06f8ece0c576bd8af59f665669bcdcb09b
Cralle <cralle@outlook.com>
2018-10-11 17:28:53 +0200

    More errors

--- skoleklima/api/api-get-sensor-info.php
+++ skoleklima/api/api-get-sensor-info.php
@@ -1,6 +1,6 @@
 <?php
 $sensor = $_POST['sensor'];
-$returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20".$sensor.");
+$returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20".$sensor);
 echo $returnFromIfx;
 return $returnFromIfx;
 ?>
