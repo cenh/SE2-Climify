e17bee0f09a792c29f61c53eb99b065311c47e35
98christianp <christianpetersen98@gmail.com>
2018-10-12 09:29:10 +0200

    moved to php view room x

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -82,6 +82,27 @@
 
 </div>
 <script>
+
+    var googleDate = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})([+-]\d{2}):(\d{2})$/;
+
+    function date_conversion(d) {
+        var m = googleDate.exec(d);
+        var year   = +m[1];
+        var month  = +m[2];
+        var day    = +m[3];
+        var hour   = +m[4];
+        var minute = +m[5];
+        var second = +m[6];
+        var msec   = +m[7];
+        var tzHour = +m[8];
+        var tzMin  = +m[9];
+        var tzOffset = new Date().getTimezoneOffset() + tzHour * 60 + tzMin;
+
+        var newDate = new Date(year, month - 1, day, hour, minute - tzOffset, second, msec).toLocaleString();
+        console.log(newDate);
+        return newDate;
+    }
+
     var obj = <?php echo $returnFromIfx; ?>;
     var json_batt = JSON.parse(obj);
     // get the latest battery reading
--- skoleklima/js/main.js
+++ skoleklima/js/main.js
@@ -802,25 +802,6 @@ function requestNewUserSetup() {
 
 }*/
 
-var googleDate = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})([+-]\d{2}):(\d{2})$/;
-
-function date_conversion(d) {
-    var m = googleDate.exec(d);
-    var year   = +m[1];
-    var month  = +m[2];
-    var day    = +m[3];
-    var hour   = +m[4];
-    var minute = +m[5];
-    var second = +m[6];
-    var msec   = +m[7];
-    var tzHour = +m[8];
-    var tzMin  = +m[9];
-    var tzOffset = new Date().getTimezoneOffset() + tzHour * 60 + tzMin;
-
-    var newDate = new Date(year, month - 1, day, hour, minute - tzOffset, second, msec).toLocaleString();
-    console.log(newDate);
-    return newDate;
-}
 
 
 
