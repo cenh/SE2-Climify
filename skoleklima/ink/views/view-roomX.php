7856c126f1c8cacc2d3fd1e29944cd1dc11c5b8f
ciokos <wciokos@gmail.com>
2018-10-12 10:18:28 +0200

    no graph :(

--- skoleklima/ink/views/view-roomX.php
+++ skoleklima/ink/views/view-roomX.php
@@ -1,14 +1,12 @@
-
-
 <div class="single-view view-roomX">
     <?php
     $returnFromIfx = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readBattery");
     $returnFromIfx = json_encode($returnFromIfx, true);
-    $temperature = file_get_contents(   "http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readOutdoorTemperature");
+    $temperature = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readOutdoorTemperature");
     $temperature = json_encode($temperature, true);
 
     $humidity = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readHumidity");
-    $humidity = json_encode($humidity,true);
+    $humidity = json_encode($humidity, true);
     ?>
     <div class="roomX-top">
         <span>
@@ -18,17 +16,17 @@
     </div>
     <hr>
 
-<!--    <div class="button-float">-->
-<!--        <button id="button2">-->
-<!--            REFRESHDATA-->
-<!--        </button>-->
-<!--    </div>-->
+    <!--    <div class="button-float">-->
+    <!--        <button id="button2">-->
+    <!--            REFRESHDATA-->
+    <!--        </button>-->
+    <!--    </div>-->
 
     <!-- TODO: view data -->
     <div class="data-display-box">
-        <div class="temperature-graph-box">
-            <canvas id="myChart"></canvas>
-        </div>
+        <!--        <div class="temperature-graph-box">-->
+        <!--            <canvas id="myChart"></canvas>-->
+        <!--        </div>-->
         <div class="information-box">
             <h3>Information Panel</h3>
             <div class="view-roomX-data-box">
@@ -43,28 +41,28 @@
                     <h4 style="float:right; padding-top:10px; padding-right:70%" id="humi_cur"></h4>
                 </span>
             </div>
-            <div class="view-roomX-data-box">
-                <i class="menu-link-ico nav-icon fa fa-volume-down" aria-hidden="true"></i>
-            </div>
-            <div class="view-roomX-data-box">
-                <span>
-                    <i class="menu-link-ico nav-icon fa fa-battery-half"></i>
-                    <h4 style="float:right; padding-top:10px; padding-right:70%" id="bat_lvl"></h4>
-                </span>
-            </div>
+            <!--            <div class="view-roomX-data-box">-->
+            <!--                <i class="menu-link-ico nav-icon fa fa-volume-down" aria-hidden="true"></i>-->
+            <!--            </div>-->
+            <!--            <div class="view-roomX-data-box">-->
+            <!--                <span>-->
+            <!--                    <i class="menu-link-ico nav-icon fa fa-battery-half"></i>-->
+            <!--                    <h4 style="float:right; padding-top:10px; padding-right:70%" id="bat_lvl"></h4>-->
+            <!--                </span>-->
+            <!--            </div>-->
         </div>
     </div>
 
-
-        <div class="view-roomX-data-box">
-            <div class="temperature">
-                <!-- TODO: -->
-                <span>
+    <div class="view-roomX-data-box">
+        <div class="temperature">
+            <!-- TODO: -->
+            <span>
                     <h4>
                         The current set temperature is: <label id="current_set_temp"></label>
                     </h4>
                 </span>
-            </div>
+        </div>
+
         <div class="view-roomX-data-box" style="border:1px solid #92b2c7; padding:15px">
            <span>
                 <form id="temp" class="my-center">
@@ -72,6 +70,12 @@
                     <input type="number" name="value">
                 </form>
            </span>
+            <div class="view-roomX-data-box" style="float: right">
+                            <span>
+                                <i class="menu-link-ico nav-icon fa fa-battery-half"></i>
+                                <h4 style="float:right; padding-top:10px; padding-right:70%" id="bat_lvl"></h4>
+                            </span>
+            </div>
         </div>
         <div class="button-float" style="float:right; padding:10px">
             <button id="my_button">
@@ -86,15 +90,15 @@
     var json_batt = JSON.parse(obj);
     // get the latest battery reading
     var latest = json_batt.results[0].series[0].values.slice(-1)[0];
-    document.getElementById("bat_lvl").innerHTML = latest[1].substring(0,4)+ "%";
+    document.getElementById("bat_lvl").innerHTML = latest[1].substring(0, 4) + "%";
     var t_obj = <?php echo $temperature; ?>;
     var json_t = JSON.parse(t_obj);
     var t_latest = json_t.results[0].series[0].values.slice(-1)[0];
-    document.getElementById("temp_cur").innerHTML = t_latest[1].substring(0,4)+ "C";
+    document.getElementById("temp_cur").innerHTML = t_latest[1].substring(0, 4) + "C";
 
     var h_obj = <?php echo $humidity; ?>;
     var json_h = JSON.parse(h_obj);
     var h_latest = json_h.results[0].series[0].values.slice(-1)[0];
-    document.getElementById("humi_cur").innerHTML = h_latest[1].substring(0,4);
+    document.getElementById("humi_cur").innerHTML = h_latest[1].substring(0, 4);
 
 </script>
