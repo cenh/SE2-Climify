862fc4dda33efbc92483befd9003ede69a908a41
ciokos <wciokos@gmail.com>
2018-10-30 13:12:58 +0100

    table28

--- skoleklima/js/main.js
+++ skoleklima/js/main.js
@@ -902,20 +902,19 @@ $("#my_button").click(function() {
 
 $(document).ready(function() {
     $('#table_id1').DataTable();
+    $('#table_id2').DataTable();
     refreshTable();
 } );
 
-$(document).ready(function() {
-    $('#table_id2').DataTable();
-} );
-
-$("#mybutton").click(function() {
-    var table1 = $('#table_id1').DataTable();
-    table1.row.add(['Jacob', 'Awesome person']).draw();
-    var table2 = $('#table_id2').DataTable();
-    table2.row.add(['Christy', 'Awesome person']).draw();
-});
+// $("#mybutton").click(function() {
+//
+// });
 
 function refreshTable(){
-  console.log("hello");
+    setInterval(function(){
+        var table1 = $('#table_id1').DataTable();
+        table1.row.add(['Jacob', 'Awesome person']).draw();
+        var table2 = $('#table_id2').DataTable();
+        table2.row.add(['Christy', 'Awesome person']).draw();
+      }, 1000);
 }
