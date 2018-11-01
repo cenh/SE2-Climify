ac42d2fb99c86307cf4070272aafa58156f2b425
kacper <kacper.zyla97@gmail.com>
2018-11-01 11:41:52 +0100

    hot fix #1

--- skoleklima/js/charts.js
+++ skoleklima/js/charts.js
@@ -292,7 +292,7 @@ function dateRangePicker1() {
             fetchingDataGraph = true;
             disableGraphSettingsSelections();
         }
-        howToDraw();
+
     }
 
     $('#reportrange1').daterangepicker({
