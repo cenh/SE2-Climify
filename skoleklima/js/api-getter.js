2745fc867fbc6f299ca11dc7c0366bb1676e2382
98christianp <christianpetersen98@gmail.com>
2018-11-02 13:20:07 +0100

    testing api calls

--- skoleklima/js/api-getter.js
+++ skoleklima/js/api-getter.js
@@ -6,6 +6,7 @@ function loadRoomDetails() {
         url: "api/api-get-current-temperature.php",
         dataType: "json",
     }).done(function (res) {
+        console.log(res);
         var json_t = JSON.parse(res);
         var t_latest = json_t.results[0].series[0].values.slice(-1)[0];
         document.getElementById("temp_cur").innerHTML = t_latest[1].substring(0, 4) + "C";
@@ -17,6 +18,7 @@ function loadRoomDetails() {
         url: "api/api-get-humidity.php",
         dataType: "json",
     }).done(function (res) {
+        console.log(res);
         var json_h = JSON.parse(res);
         var h_latest = json_h.results[0].series[0].values.slice(-1)[0];
         document.getElementById("humi_cur").innerHTML = h_latest[1].substring(0, 4);
