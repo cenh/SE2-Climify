c6f637028c805e743a526b0313793e8ef88ae2d7
98christianp <christianpetersen98@gmail.com>
2018-11-02 11:31:36 +0100

    api call on loop every 10 sec

--- skoleklima/api/api-get-humidity.php
+++ skoleklima/api/api-get-humidity.php
@@ -1,4 +1,4 @@
 <?php
 $humidity = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20value%20FROM%20readHumidity");
-$humidity = json_encode($humidity, true);
+echo $humidity;
 return $humidity;
