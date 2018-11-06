a90a9090b16ac9af04040d6ab5f2c9bb0b1fd7ef
98christianp <christianpetersen98@gmail.com>
2018-11-06 17:45:26 +0100

    refractored api - fixing 4

--- skoleklima/js/api-getter.js
+++ skoleklima/js/api-getter.js
@@ -1,10 +1,10 @@
 
 function loadRoomDetails() {
-    console.log("ROOM DETAILS....");
+    //console.log("ROOM DETAILS....");
     $.post("api/api-get-all-sensordata.php", {'sensor':'readOutdoorTemperature'})
         .done(function (res) {
             console.log(res);
-            var t_latest = res.results[0].series[0].values.slice(-1)[0];
+            //var t_latest = res.results[0].series[0].values.slice(-1)[0];
             document.getElementById("temp_cur").innerHTML = t_latest[1].substring(0, 4) + " ℃";
         }).fail(function (jqXHR, textStatus, errorThrown) {
         alert("AJAX call failed: " + textStatus + ", " + errorThrown);
@@ -22,7 +22,7 @@ function loadRoomDetails() {
     }).fail(function (jqXHR, textStatus, errorThrown) {
         alert("AJAX call failed: " + textStatus + ", " + errorThrown);
     });*/
-    $.ajax({
+/*    $.ajax({
         type: "POST",
         url: "api/api-get-all-sensordata.php",
         data: {'sensor': 'readHumidity'},
@@ -31,5 +31,5 @@ function loadRoomDetails() {
         console.log(res);
         var h_latest = res.results[0].series[0].values.slice(-1)[0];
         document.getElementById("humi_cur").innerHTML = h_latest[1].substring(0, 4);
-    });
+    });*/
 }
