e0d916bf6e8f51bd96bb4a5bf7331e18fc4ecfa5
Cralle <cralle@outlook.com>
2018-11-06 10:47:36 +0100

    PHP Ruler

---
+++ skoleklima/api/api-rule-execute.php
@@ -0,0 +1,25 @@
+<?php
+//************************************************
+//	Get influx database info
+//************************************************
+
+require_once "../meta.php";
+
+$rb = new RuleBuilder;
+$rule = $rb->create(
+  $rb['temperature']->greaterThanOrEqualTo($rb['actualTemperature'])
+  ),
+  function() {
+    error_log("RULE_EXECUTED!!!!!", 0);
+  }
+);
+
+$context = new Context(array(
+  'temperature' => 25,
+  'actualTemperature' => function() {
+      return 20;
+    },
+));
+
+$rule->execute($context);
+ ?>
