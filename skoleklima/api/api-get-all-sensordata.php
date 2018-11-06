86339cb81cfb31799bc9dfd81eac8714d06b86b5
98christianp <christianpetersen98@gmail.com>
2018-11-06 17:55:46 +0100

    refractored api - fixing 4

--- skoleklima/api/api-get-all-sensordata.php
+++ skoleklima/api/api-get-all-sensordata.php
@@ -15,4 +15,4 @@ switch ($sensor) {
     // TODO: any more things need to be called from this api?? (Hasn't been tested)
 $data = file_get_contents(   "http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20".$dbVariable ."FROM%20".$sensor);
 echo $data;
-return $data;
+return $body;
