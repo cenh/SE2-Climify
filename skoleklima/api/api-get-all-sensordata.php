ed9af983f194f0742b78d8631afd67e7b7558fd4
98christianp <christianpetersen98@gmail.com>
2018-11-06 18:54:43 +0100

    still refractor api fix

--- skoleklima/api/api-get-all-sensordata.php
+++ skoleklima/api/api-get-all-sensordata.php
@@ -13,4 +13,5 @@ $body = file_get_contents('php://input');
 
     // TODO: any more things need to be called from this api?? (Hasn't been tested)
 //$data = file_get_contents(   "http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20".$dbVariable ."FROM%20".$body);
+echo $body;
 return $body;
