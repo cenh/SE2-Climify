c7008a01bda398ff320840c8a8da06c4d907e5ca
ciokos <wciokos@gmail.com>
2018-11-06 10:14:03 +0100

    table_admin9

--- skoleklima/admin-login/ink/dashboard/view-devices.php
+++ skoleklima/admin-login/ink/dashboard/view-devices.php
@@ -2,8 +2,6 @@
 
 <div class="viewport">
 
-
-
     <div class="viewport-dashboard"> 
         <div class="top-btn-wrapper">
             <span>
@@ -28,22 +26,4 @@
             <!-- Content goes here -->
         </div>
     </div>
-</div>
-
-<div style="width: 100%; text-align: center;
-         font-weight: bold; font-size: 150%;"> Roles </div>
-
-<div style="width: 100%; height: 50%; border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px; float:left;">
-    <table id="roles_table" class="display">
-        <thead>
-        <tr>
-            <th>Name</th>
-        </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
 </div>
