eaab6d9a634e601fee33208381f0254cc9397f10
ciokos <wciokos@gmail.com>
2018-11-06 12:02:28 +0100

    table_admin31

--- skoleklima/admin-login/api/api-get-roles.php
+++ skoleklima/admin-login/api/api-get-roles.php
@@ -4,40 +4,94 @@
 require_once "../admin-meta.php";
 require_once "../session.php";
 
+$phaseSessionToken = clean($_POST[sessionToken]);
+
+if( $phaseSessionToken != $adminSessionToken ){
+    echo '{"status":"error"}';
+    exit;
+}
+
+if (!$systemAccess) {
+    echo '{"status":"error"}';
+    exit;
+}
+
+$phaseStatus=clean($_POST["status"]); //Blocked
+$phaseSearch=clean($_POST["search"]);
+
 
 $servername = DB_HOST;
 $username = DB_USER;
 $password = DB_PASSWORD;
 $databasename = DB_NAME;
-
+$pepper = HASH_PEPPER;
 
 $conn = new mysqli($servername, $username, $password, $databasename);
 if ($conn->connect_error) {
     die("Connection error: " . $conn->connect_error);
+    exit;
 }
 
-$query = "SELECT * FROM Role;";
-$stmt = $mysqli->stmt_init();
 
+$stmt = $conn->prepare("(SELECT * FROM Role);");
+
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$emparray = array();
+while($row = mysqli_fetch_assoc($result))
+{
+    $emparray[] = $row;
+}
+
+$messages = json_encode( $emparray , JSON_UNESCAPED_UNICODE );
+echo $messages;
+
+$stmt->close();
+
+$conn->close();
+
+?>
+
+<?php
+
+$mysqli = new mysqli("127.0.0.1", "user", "password", "world");
+
+if($mysqli->connect_error)
+{
+    die("$mysqli->connect_errno: $mysqli->connect_error");
+}
+
+$query = "SELECT Name, Population, Continent FROM Country WHERE Continent=? ORDER BY Name LIMIT 1";
+
+$stmt = $mysqli->stmt_init();
 if(!$stmt->prepare($query))
 {
     print "Failed to prepare statement\n";
 }
-else {
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $emparray = array();
-    while($row = mysqli_fetch_assoc($result))
-    {
-        $emparray[] = $row;
-    }
+else
+{
+    $stmt->bind_param("s", $continent);
 
-    $messages = json_encode( $emparray , JSON_UNESCAPED_UNICODE );
-    echo $messages;
+    $continent_array = array('Europe','Africa','Asia','North America');
+
+    foreach($continent_array as $continent)
+    {
+        $stmt->execute();
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_array(MYSQLI_NUM))
+        {
+            foreach ($row as $r)
+            {
+                print "$r ";
+            }
+            print "\n";
+        }
+    }
 }
 
-
 $stmt->close();
-$conn->close();
-
+$mysqli->close();
 ?>
