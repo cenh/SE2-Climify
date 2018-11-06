50969d99c2cd5fc1a1e4c52906e6dae1ce4d73fd
ciokos <wciokos@gmail.com>
2018-11-06 11:53:03 +0100

    table_admin28

--- skoleklima/admin-login/api/api-get-roles.php
+++ skoleklima/admin-login/api/api-get-roles.php
@@ -4,53 +4,40 @@
 require_once "../admin-meta.php";
 require_once "../session.php";
 
-$phaseSessionToken = clean($_POST[sessionToken]);
-
-if( $phaseSessionToken != $adminSessionToken ){
-    echo '{"status":"error"}';
-    exit;
-}
-
-if (!$systemAccess) {
-    echo '{"status":"error"}';
-    exit;
-}
-
-$phaseStatus=clean($_POST["status"]); //Blocked
-$phaseSearch=clean($_POST["search"]);
-
 
 $servername = DB_HOST;
 $username = DB_USER;
 $password = DB_PASSWORD;
 $databasename = DB_NAME;
-$pepper = HASH_PEPPER;
+
 
 $conn = new mysqli($servername, $username, $password, $databasename);
 if ($conn->connect_error) {
     die("Connection error: " . $conn->connect_error);
-    exit;
 }
 
+$query = "SELECT * FROM Role";
+$stmt = $mysqli->stmt_init();
 
-$stmt = $conn->prepare("(SELECT * FROM Role);");
-
-
-$stmt->execute();
-
-$result = $stmt->get_result();
-
-$emparray = array();
-while($row = mysqli_fetch_assoc($result))
+if(!$stmt->prepare($query))
 {
-    $emparray[] = $row;
+    print "Failed to prepare statement\n";
+}
+else {
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $emparray = array();
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+
+    $messages = json_encode( $emparray , JSON_UNESCAPED_UNICODE );
+    echo $messages;
 }
 
-$messages = json_encode( $emparray , JSON_UNESCAPED_UNICODE );
-echo $messages;
 
 $stmt->close();
-
 $conn->close();
 
 ?>
