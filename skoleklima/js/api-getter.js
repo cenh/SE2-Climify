b9cebd6b5c24a5aa6de0564c371ffb233b9d46f2
98christianp <christianpetersen98@gmail.com>
2018-11-06 18:26:10 +0100

    refractored api - fixing 4

--- skoleklima/js/api-getter.js
+++ skoleklima/js/api-getter.js
@@ -1,9 +1,16 @@
 
 function loadRoomDetails() {
     //console.log("ROOM DETAILS....");
+    $.post("api/api-get-all-sensordata.php", {'sensor':'readOutdoorTemperature'},
+        function (res) {
+            console.log(res);
+            var t_latest = res.results[0].series[0].values.slice(-1)[0];
+            document.getElementById("temp_cur").innerHTML = t_latest[1].substring(0, 4) + " ℃";
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert("AJAX call failed: " + textStatus + ", " + errorThrown);
+    });
 
-
-    $.ajax({
+    /*$.ajax({
         type: "POST",
         url: "api/api-get-all-sensordata.php",
         data: {'sensor': 'readOutdoorTemperature'},
@@ -14,8 +21,8 @@ function loadRoomDetails() {
         document.getElementById("temp_cur").innerHTML = t_latest[1].substring(0, 4) + " ℃";
     }).fail(function (jqXHR, textStatus, errorThrown) {
         alert("AJAX call failed: " + textStatus + ", " + errorThrown);
-    });
-    $.ajax({
+    });*/
+/*    $.ajax({
         type: "POST",
         url: "api/api-get-all-sensordata.php",
         data: {'sensor': 'readHumidity'},
@@ -24,5 +31,5 @@ function loadRoomDetails() {
         console.log(res);
         var h_latest = res.results[0].series[0].values.slice(-1)[0];
         document.getElementById("humi_cur").innerHTML = h_latest[1].substring(0, 4);
-    });
+    });*/
 }
