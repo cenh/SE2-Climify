fb225580233820a3520c6c786743129db7e190bc
ciokos <wciokos@gmail.com>
2018-11-06 10:44:17 +0100

    table_admin17

--- skoleklima/admin-login/ink/header.php
+++ skoleklima/admin-login/ink/header.php
@@ -8,3 +8,21 @@
 		</div>
     </div>
 </header>
+
+<div style="width: 100%; text-align: center;
+         font-weight: bold; font-size: 150%;"> Roles </div>
+
+<div style="width: 100%; height: 50%; border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px; float:left;">
+    <table id="roles_table" class="display">
+        <thead>
+        <tr>
+            <th>Name</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</div>
