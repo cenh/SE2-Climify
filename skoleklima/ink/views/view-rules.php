63e1d55f8529c8f239c5c5a11fea09c8617cbcd7
98christianp <christianpetersen98@gmail.com>
2018-11-08 14:08:02 +0100

    test : rules (HELLO WORLD)

--- skoleklima/ink/views/view-rules.php
+++ skoleklima/ink/views/view-rules.php
@@ -1,6 +1,36 @@
 <!-- show rules -->
 <div class="single-view view-rules">
-    <div class="view-rules-top">
-	    <h1> Hello World! Rules Works!</h1>
+    <div class="view-data-top">
+	<span>
+		<h3>Rules</h3>
+		<p>View rules and toggle them (on/off) for a chosen location</p>
+	</span>
+        <span>
+		<span>
+			<?php if ( $currentUserRole == 1 || $currentUserRole == 15 ) { ?>
+                <select class="list-schools-other-users" name="option" id="graphMainSelect">
+					<option value="" selected>Choose Institution</option>
+                    <!-- Content goes here -->
+				</select>
+            <?php } ?>
+
+            <select class="chart-select-map" >
+				<!-- Content goes here -->
+				<option value="stand" selected>Choose Floor plan</option>
+			</select>
+
+			<button id="btn-toggle-chart-info" class="link"></button>
+		</span>
+		<span>
+			<select class="chart-select-location" id="selectLocation">
+			<option value="locStand" selected>Choose Location</option>
+                <!-- Content goes here -->
+			</select>
+
+
+
+		</span>
+	</span>
     </div>
+
 </div>
