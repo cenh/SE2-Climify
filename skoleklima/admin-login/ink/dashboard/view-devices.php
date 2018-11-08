f9fd82e8aadefc65461adcac72e197a6493e519e
ciokos <wciokos@gmail.com>
2018-11-08 12:02:56 +0100

    table_admin49

--- skoleklima/admin-login/ink/dashboard/view-devices.php
+++ skoleklima/admin-login/ink/dashboard/view-devices.php
@@ -1,7 +1,6 @@
 <!-- View dahsboard -->
 
 <div class="viewport">
-
     <div class="viewport-dashboard"> 
         <div class="top-btn-wrapper">
             <span>
@@ -25,5 +24,29 @@
             <p><i class="update-spinner fa fa-spinner fa-spin fa-1x fa-fw"></i> Fetching data...</p>
             <!-- Content goes here -->
         </div>
+    </div>
+</div>
+
+
+<div style="height: 100px"></div>
+
+
+<div style="height: auto">
+    <div style="width: 100%; text-align: center;
+         font-weight: bold; font-size: 150%;"> Roles </div>
+
+    <div style="width: 100%; height: 400px; border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px; float:left;">
+        <table id="roles_table" class="display">
+            <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     </div>
 </div>
--- skoleklima/admin-login/ink/header.php
+++ skoleklima/admin-login/ink/header.php
@@ -1,23 +1,5 @@
 <!-- Header -->
-<div style="height: auto">
-    <div style="width: 100%; text-align: center;
-         font-weight: bold; font-size: 150%;"> Roles </div>
 
-    <div style="width: 100%; height: 400px; border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px; float:left;">
-        <table id="roles_table" class="display">
-            <thead>
-            <tr>
-                <th>Name</th>
-            </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-    </div>
-</div>
 <header>
     <div class="header-content">
 		<p class="software-name-header-wrapper">System Administration</p>
