aa79114bca6709ab54b8a4bc64524c3dcfb44f77
98christianp <christianpetersen98@gmail.com>
2018-11-08 15:34:50 +0100

    cloning the data view to rules

--- skoleklima/ink/views/view-rules.php
+++ skoleklima/ink/views/view-rules.php
@@ -1,32 +1,20 @@
+<?php
+    $conn = mysqli_connect($DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $loc_query = "SELECT LocationID From 'Location''"
+
+    $result1 = mysqli_query($conn,$loc_query);
+?>
+
 <!-- show rules -->
 <div class="single-view view-rules">
     <div class="view-data-top">
 	<span>
 		<h3>Rules</h3>
 		<p>View rules and toggle them (on/off) for a chosen location</p>
-        <span>
-		<span>
-			<?php if ( $currentUserRole == 1 || $currentUserRole == 15 ) { ?>
-                <select class="list-schools-other-users" name="option" id="graphMainSelect">
-					<option value="" selected>Choose Institution</option>
-                    <!-- Content goes here -->
-				</select>
-            <?php } ?>
-
-            <select class="chart-select-map" >
-				<!-- Content goes here -->
-				<option value="stand" selected>Choose Floor plan</option>
-			</select>
-		</span>
-		<span>
-			<select class="chart-select-location" id="selectLocation">
-
-			</select>
-		</span>
-        <button id="fetch-sensors-for-loc" class="link">Get sensors</button>
-	</span>
-    </div>
-    <hr>
-	</span>
+        <select>
+            <?php while ($row1 = mysql_fetch_array($result1)):;?>
+            <option value="<?php echo $row[1];?>"></option>
+            <?php endwhle;?>
+        </select>
     </div>
 </div>
