81e5c19b11adc054b7921b280a6b4a2e158736ee
ciokos <wciokos@gmail.com>
2018-11-08 13:07:12 +0100

    table_admin54

--- skoleklima/admin-login/ink/dashboard/view-devices.php
+++ skoleklima/admin-login/ink/dashboard/view-devices.php
@@ -1,28 +1,5 @@
 <!-- View dahsboard -->
 
-<div style="height: 100px"></div>
-
-
-<div style="height: auto">
-    <div style="width: 100%; text-align: center;
-         font-weight: bold; font-size: 150%;"> Roles </div>
-
-    <div style="width: 100%; height: 400px; border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px; float:left;">
-        <table id="roles_table" class="display">
-            <thead>
-            <tr>
-                <th>Name</th>
-            </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-    </div>
-</div>
-
 <div class="viewport">
     <div class="viewport-dashboard"> 
         <div class="top-btn-wrapper">
@@ -80,4 +57,23 @@
     </div>
 </div>
 
+<div style="height: auto">
+    <div style="width: 100%; text-align: center;
+         font-weight: bold; font-size: 150%;"> Roles </div>
+
+    <div style="width: 100%; height: auto; border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px; float:left;">
+        <table id="roles_table" class="display">
+            <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div>
 
