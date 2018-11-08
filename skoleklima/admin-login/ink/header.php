4073c4aa28f4a36292f37f2c7217a8a837224c35
ciokos <wciokos@gmail.com>
2018-11-08 11:54:03 +0100

    table_admin48

--- skoleklima/admin-login/ink/header.php
+++ skoleklima/admin-login/ink/header.php
@@ -1,14 +1,4 @@
 <!-- Header -->
-<header>
-    <div class="header-content">
-		<p class="software-name-header-wrapper">System Administration</p>
-		<div class="link" id="btn-sign-out">
-			<p>Sign out</p>
-			<i class="fa fa-sign-out" aria-hidden="true"></i>
-		</div>
-    </div>
-</header>
-
 <div style="height: auto">
     <div style="width: 100%; text-align: center;
          font-weight: bold; font-size: 150%;"> Roles </div>
@@ -28,3 +18,14 @@
         </table>
     </div>
 </div>
+<header>
+    <div class="header-content">
+		<p class="software-name-header-wrapper">System Administration</p>
+		<div class="link" id="btn-sign-out">
+			<p>Sign out</p>
+			<i class="fa fa-sign-out" aria-hidden="true"></i>
+		</div>
+    </div>
+</header>
+
+
