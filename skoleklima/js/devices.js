449592837662aacafb6b7378de8445760c99dad4
ciokos <wciokos@gmail.com>
2018-11-13 10:34:23 +0100

    devices list 2

--- skoleklima/js/devices.js
+++ skoleklima/js/devices.js
@@ -1,1 +1,31 @@
-console.log('message');
+$(document).ready(function () {
+    $('#table_id1').DataTable();
+    getTableData();
+    refreshTable();
+});
+
+function refreshTable() {
+    setInterval(function () {
+        getTableData();
+    }, 10000);
+}
+
+function getTableData() {
+    $.ajax({
+        type: "GET",
+        url: "api/api-get-sensor-info.php",
+        dataType: "json",
+
+    }).done(function (res) {
+        var names = res.results[0].series[0].values;
+        var table1 = $('#table_id1').DataTable();
+        table1.clear();
+        for(var i = 0; i < names.length; i++) {
+            table1.row.add([names[i]]).draw(false);
+        }
+
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert("AJAX call failed: " + textStatus + ", " + errorThrown);
+    });
+
+}
