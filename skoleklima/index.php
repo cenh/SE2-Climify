e7c502d529150f324c84a1b2b16f5dcedc336ae3
ciokos <wciokos@gmail.com>
2018-11-13 10:22:39 +0100

    cleaning 1

--- skoleklima/index.php
+++ skoleklima/index.php
@@ -89,6 +89,8 @@
 
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 
+    <script type="text/javascript" src="js/devices.js"></script>
+
 	<?php
 		if ( $currentUserID ) { ?>
 
