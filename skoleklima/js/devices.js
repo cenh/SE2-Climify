f1f0b8aa305c9e4a2baf1ffcf39ec192bbe8a250
ciokos <wciokos@gmail.com>
2018-11-13 14:07:18 +0100

    devices list 34

--- skoleklima/js/devices.js
+++ skoleklima/js/devices.js
@@ -24,8 +24,11 @@ $(document).ready(function () {
 
         }
     });
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
+
 });
 
 function refreshTable(element) {
