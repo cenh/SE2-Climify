4b7ffdbc9fe8eb0065bd17e7d009fdc0c723c593
98christianp <christianpetersen98@gmail.com>
2018-11-14 14:37:51 +0100

    changed variable name

--- skoleklima/js/main.js
+++ skoleklima/js/main.js
@@ -890,25 +890,6 @@ $(document).ready(function () {
     refreshTable();
 });
 // RULES FUNCTIONALITY BELOW
-$("#fetch-sensors-for-loc").on("click",function() {
-    if ($("#sel_location").val()==="1") {
-        var room_selected = $("#sel_location").val();
-        console.log(room_selected);
-        $.post('api/api-rules-query.php', {
-            room: room_selected
-        }).done(function (res) {
-            $(".ui-button-text").text("Room: " + res);
-            $("#rules-title").text("Rules for room " + res);
-        });
-        $(".collapsible").show();
-
-    }
-    else if ($("#sel_location").val()==="2") {
-        console.log("2");
-        $(".collapsible").hide();
-        alert("No rules specefied for selected room");
-    }
-});
 $("#rulecheck").click(function(){
     isChecked = $("#rulecheck").is(":checked");
     console.log(isChecked);
--- skoleklima/js/rule-getter.js
+++ skoleklima/js/rule-getter.js
@@ -1,11 +1,29 @@
 
-$("#fetch-sensors-for-loc").click(function () {
-    var rule_Location = $("#selectLocation").val();
 
-    // We
+$("#fetch-sensors-for-loc").on("click",function() {
+    var rule_Location = $("#sel_location").val();
+
     $.get('api/api-get-rules.php', {LocationID: rule_Location})
         .done(function(res) {
             var rules = res;
             console.log(rules);
         });
+
+    if ($("#sel_location").val()==="1") {
+        var room_selected = $("#sel_location").val();
+        console.log(room_selected);
+        $.post('api/api-rules-query.php', {
+            room: room_selected
+        }).done(function (res) {
+            $(".ui-button-text").text("Room: " + res);
+            $("#rules-title").text("Rules for room " + res);
+        });
+        $(".collapsible").show();
+
+    }
+    else if ($("#sel_location").val()==="2") {
+        console.log("2");
+        $(".collapsible").hide();
+        alert("No rules specefied for selected room");
+    }
 });
