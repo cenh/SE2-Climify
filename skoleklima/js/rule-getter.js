8931838645781f9ffd15d86d530f01286e4e5ced
98christianp <christianpetersen98@gmail.com>
2018-11-14 14:19:36 +0100

    using print_r  instead of var_dump

--- skoleklima/js/rule-getter.js
+++ skoleklima/js/rule-getter.js
@@ -2,11 +2,10 @@
 $("#fetch-sensors-for-loc").click(function () {
     var location = $("#selectLocation").val();
 
-
     // We
     $.get('api/api-get-rules.php', {LocationID: location})
         .done(function(res) {
             var rules = res;
-            console.log(rules)
-        })
+            console.log(rules);
+        });
 });
