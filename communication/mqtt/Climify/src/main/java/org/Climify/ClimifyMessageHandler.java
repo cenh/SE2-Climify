075fda6ab97c00a16c79cb4bf951b5e9d2ffbe31
Cralle <cralle@outlook.com>
2018-11-16 16:24:59 +0100

    Updated

--- communication/mqtt/Climify/src/main/java/org/Climify/ClimifyMessageHandler.java
+++ communication/mqtt/Climify/src/main/java/org/Climify/ClimifyMessageHandler.java
@@ -1,6 +1,11 @@
 package org.Climify;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.Climify.influxDB.InfluxCommunicator;
 import org.Climify.mariaDB.MariaDBCommunicator;
@@ -30,6 +35,7 @@ public class ClimifyMessageHandler extends MessageHandler {
 				System.out.println("Inside topic " + topic); 
 				SensorMeasurement measurement = dslJson.deserialize(SensorMeasurement.class, message.getPayload(), message.getPayload().length);
 				influx.saveMeasurement(measurement);
+				executeRule(measurement.name);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -47,6 +53,29 @@ public class ClimifyMessageHandler extends MessageHandler {
 				e.printStackTrace();
 			}
 			
+		}
+	}
+
+	public void executeRule(String SensorID) {
+		MariaDBCommunicator mb = new MariaDBCommunicator();
+		List<List<String>> results = new ArrayList<List<String>>();
+		results = mb.getRulesBySensorID(SensorID);
+		for(List<String> result : results) {
+			try {
+				URL url = new URL("http://localhost:8080/api/api-execute-rule.php");
+				HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+				httpCon.setDoOutput(true);
+				httpCon.setRequestMethod("POST");
+				PrintStream ps = new PrintStream(httpCon.getOutputStream());
+				ps.print("sensorID=" + result.get(0));
+				ps.print("&Operator=" + result.get(1));
+				ps.print("&Value=" + result.get(2));
+				ps.print("&Action=" + result.get(3));
+				httpCon.getInputStream();
+				ps.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
