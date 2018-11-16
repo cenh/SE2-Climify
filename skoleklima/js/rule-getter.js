156a0891950c8133419779a57489f5945198b9b4
98christianp <christianpetersen98@gmail.com>
2018-11-16 02:44:27 +0100

    jesus get back to normal

--- skoleklima/js/rule-getter.js
+++ skoleklima/js/rule-getter.js
@@ -40,10 +40,23 @@ $("#fetch-sensors-for-loc").on("click",function() {
         $(".ui-button-text").text("Room: " + res);
         $("#rules-title").text("Rules for room " + res);
     });
-    if (rules){
-        $(".collapsible").show();
-    } else {
-        $(".collapsible").hide();
-    }
 
+    if ($("#sel_location").val()==="1") {
+        var room_selected = $("#sel_location").val();
+        console.log(room_selected);
+        $.post('api/api-rules-query.php', {
+            room: room_selected
+        }).done(function (res) {
+            $(".ui-button-text").text("Room: " + res);
+            $("#rules-title").text("Rules for room " + res);
+        });
+        $(".collapsible").show();
+
+    }
+    else if ($("#sel_location").val()==="2") {
+        console.log("2");
+        $(".collapsible").hide();
+        alert("No rules specefied for selected room");
+    }
+});
 });
