097f8954298f7befc77b2615ebf74d7c772e656a
ciokos <wciokos@gmail.com>
2018-11-16 23:27:46 +0100

    devices list 56

--- skoleklima/api/api-get-devices.php
+++ skoleklima/api/api-get-devices.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
     die("Connection error: " . $conn->connect_error);
 }
 
-$query = "SELECT * FROM Things JOIN RaspberryPis ON Things.LocationID = RaspberryPis.LocationID WHERE LocationID = $roomID";
+$query = "SELECT * FROM Things JOIN RaspberryPis ON Things.RaspberryPiUID = RaspberryPis.UID";
 
 $stmt = $conn->prepare($query);
 
