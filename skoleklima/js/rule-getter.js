27350cc1613c3ca48ffe5ee4a5a053706afc37b9
98christianp <christianpetersen98@gmail.com>
2018-11-16 13:46:54 +0100

    testing accordion class

--- skoleklima/js/rule-getter.js
+++ skoleklima/js/rule-getter.js
@@ -11,7 +11,7 @@ function generateDivs(sensor, operator, value, action, ruleNo){
                 '</button>',
                 '</h5>',
             '</div>',
-            '<div id="'+body_id+'" class="collapse show" aria-labelledby="'+header_id+'" data-parent="#accordionExample">',
+            '<div id="'+body_id+'" class="collapse" aria-labelledby="'+header_id+'" data-parent="#accordionExample">',
                 '<div class="card-body">',
                     message,
                 '</div>',
@@ -25,36 +25,11 @@ $("#fetch-sensors-for-loc").on("click",function() {
     var rule_Location = $("#sel_location").val();
 
     $.get('api/api-get-rules.php', {LocationID: rule_Location})
-        .done(function(res) {
+        .done(function (res) {
             var rules = JSON.parse(res);
-            for(i=0;i < rules.length;i++){
+            for (i = 0; i < rules.length; i++) {
                 var rule = rules[i];
-                generateDivs(rule.SensorID, rule.Operator, rule.Value, rule.Action,i)
+                generateDivs(rule.SensorID, rule.Operator, rule.Value, rule.Action, i)
             }
         });
-    var room_selected = $("#sel_location").val();
-    $.post('api/api-rules-query.php', {
-        room: room_selected
-    }).done(function (res) {
-        $(".ui-button-text").text("Room: " + res);
-        $("#rules-title").text("Rules for room " + res);
-    });
-
-    if ($("#sel_location").val()==="1") {
-        var room_selected = $("#sel_location").val();
-        console.log(room_selected);
-        $.post('api/api-rules-query.php', {
-            room: room_selected
-        }).done(function (res) {
-            $(".ui-button-text").text("Room: " + res);
-            $("#rules-title").text("Rules for room " + res);
-        });
-        $(".collapsible").show();
-
-    }
-    else if ($("#sel_location").val()==="2") {
-        console.log("2");
-        $(".collapsible").hide();
-        alert("No rules specefied for selected room");
-    }
 });
