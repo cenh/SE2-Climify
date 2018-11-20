e58d14cd3ccc5485477e03700bda584287ba12fd
98christianp <christianpetersen98@gmail.com>
2018-11-20 14:23:14 +0100

    generating rulecreation form

--- skoleklima/js/rule-getter.js
+++ skoleklima/js/rule-getter.js
@@ -44,11 +44,6 @@ function generateRuleForms(){
             }
         });
 
-    html = [
-        '<select id="opSelect">',
-            '<option value="'opArray+
-        '</select>'
-    ].join('');
 
 }
 
