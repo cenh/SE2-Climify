76188cf54a80cf7487248e28875df78d1fa44d9c
Cralle <cralle@outlook.com>
2018-11-20 10:27:05 +0100

    rule apis

---
+++ skoleklima/api/api-rule-update.php
@@ -0,0 +1,28 @@
+<?php
+require_once "../meta.php";
+
+if($currentUserID == ""){
+  echo '{"status":"No UserID"}';
+	exit;
+}
+
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASSWORD;
+$databasename = DB_NAME;
+$LocationID = $_POST['RuleID'];
+$SensorID = $_POST['SensorID'];
+$Operator = $_POST['Operator'];
+$Value = $_POST['Value'];
+$Action = $_POST['Action'];
+
+$conn = new mysqli($servername, $username, $password, $databasename);
+if ($conn->connect_error) {
+  echo '{"status":"Connection died"}';
+  die("Connection error: " . $conn->connect_error);
+  exit;
+}
+
+$q = "UPDATE Rule SET SensorID=$SensorID, Operator=$Operator, Value=$Value, Action=$Action WHERE RuleID=$RuleID";
+$result = mysqli_query($conn, $q) or die("Error in Selecting " . mysqli_error($conn));
+?>
