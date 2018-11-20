1c0b58e912208cbe86b0ffa4ddedb66cde06ea5a
98christianp <christianpetersen98@gmail.com>
2018-11-20 10:31:32 +0100

    rules page v0.7.1

--- skoleklima/js/rule-getter.js
+++ skoleklima/js/rule-getter.js
@@ -1,65 +1,64 @@
-$(document).ready(function () {
-    function generateDivs(sensor, operator, value, action, ruleNo, ruleCount){
-        var message = "if " +sensor+" is "+operator + " than " +value + " then " + action.toUpperCase();
-        var header_id = 'heading'+ruleNo;
-        var body_id = 'collapse'+ruleNo;
-        var html = [
-            '<div class="card">',
-            '<div class="card-header" id="'+header_id+'">',
-            '<h5 class="mb-0">',
-            '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#'+body_id+'" aria-expanded="true" aria-controls="'+body_id+'">',
-            'Rule '+(Number(ruleNo)+1).toString(),
-            '</button>',
-            '</h5>',
-            '</div>',
-            '<div id="'+body_id+'" class="collapse" aria-labelledby="'+header_id+'" data-parent="#accordionExample">',
-            '<div class="card-body" id="'+ruleNo+'">',
-            '<div class="row">',
-            '<div class="col-sm-8">',
-            message,
-            '</div>',
-            '<div class="col-sm-4">',
-            '<button class="mybtn" style="float:right">',
-            '<i class="fa fa-trash" aria-hidden="true"></i>',
-            '</button>',
-            '</div>',
-            '</div>',
-            '</div>',
-            '</div>',
-            '</div>'
+function generateDivs(sensor, operator, value, action, ruleNo, ruleCount){
+    var message = "if " +sensor+" is "+operator + " than " +value + " then " + action.toUpperCase();
+    var header_id = 'heading'+ruleNo;
+    var body_id = 'collapse'+ruleNo;
+    var html = [
+        '<div class="card">',
+        '<div class="card-header" id="'+header_id+'">',
+        '<h5 class="mb-0">',
+        '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#'+body_id+'" aria-expanded="true" aria-controls="'+body_id+'">',
+        'Rule '+(Number(ruleNo)+1).toString(),
+        '</button>',
+        '</h5>',
+        '</div>',
+        '<div id="'+body_id+'" class="collapse" aria-labelledby="'+header_id+'" data-parent="#accordionExample">',
+        '<div class="card-body" id="'+ruleNo+'">',
+        '<div class="row">',
+        '<div class="col-sm-8">',
+        message,
+        '</div>',
+        '<div class="col-sm-4">',
+        '<button class="mybtn" style="float:right">',
+        '<i class="fa fa-trash" aria-hidden="true"></i>',
+        '</button>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>'
 
-        ].join('');
-        $(".accordion").append(html);
+    ].join('');
+    $(".accordion").append(html);
 
-    }
+}
 
-    function clearAccordion(size){
-        $(".accordion").empty();
-        $("#rule-count").text("Rules: "+size);
-    }
+function clearAccordion(size){
+    $(".accordion").empty();
+    $("#rule-count").text("Rules: "+size);
+}
 
-    var rulelocationChosen = function() {
-        return $("#sel_location").val();
-    };
+var rulelocationChosen = function() {
+    return $("#sel_location").val();
+};
 
-    $("#fetch-sensors-for-loc").on("click",function() {
-        var rule_Location = $("#sel_location").val();
+$("#fetch-sensors-for-loc").on("click",function() {
+    var rule_Location = $("#sel_location").val();
 
-        $.get('api/api-get-rules.php', {LocationID: rule_Location})
-            .done(function(res) {
-                var rules = JSON.parse(res);
-                var size = rules.length;
-                clearAccordion(size);
-                for(i=0;i < rules.length;i++){
-                    var rule = rules[i];
-                    generateDivs(rule.SensorID, rule.Operator, rule.Value, rule.Action,i,size);
-                }
-            });
-    });
+    $.get('api/api-get-rules.php', {LocationID: rule_Location})
+        .done(function(res) {
+            var rules = JSON.parse(res);
+            var size = rules.length;
+            clearAccordion(size);
+            for(i=0;i < rules.length;i++){
+                var rule = rules[i];
+                generateDivs(rule.SensorID, rule.Operator, rule.Value, rule.Action,i,size);
+            }
+        });
+});
 
-    $(".mybtn").on("click",function() {
-        console.log("hello");
-        console.log(rulelocationChosen());
-    });
+$(".mybtn").on("click",function() {
+    var thisRoom = rulelocationChosen();
+
+    console.log(thisRoom);
 });
 
