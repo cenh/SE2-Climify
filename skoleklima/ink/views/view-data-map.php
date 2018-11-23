d6fc81217cd65117cff7613f2c6c9e6c742f6ea9
ciokos <wciokos@gmail.com>
2018-11-23 15:20:41 +0100

    devices list 97

--- skoleklima/ink/views/view-data-map.php
+++ skoleklima/ink/views/view-data-map.php
@@ -26,11 +26,9 @@
     </div>
 
     <div style="height: auto">
-        <div class="input-field col s12">
-            <select id="select_room" onchange="refreshTableSensorsAndActuators(value)">
-                <option value="" disabled selected>Choose Your Room</option>
-            </select>
-        </div>
+        <select id="select_room" onchange="refreshTableSensorsAndActuators(value)">
+            <option value="" disabled selected>Choose Your Room</option>
+        </select>
     </div>
 
     <div style="overflow: auto">
