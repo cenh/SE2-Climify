ed048815c13da4bb26b088f8e6176cd48770ed94
Cralle <cralle@outlook.com>
2018-11-23 15:53:02 +0100

    Print query

--- skoleklima/api/api-get-rules.php
+++ skoleklima/api/api-get-rules.php
@@ -29,6 +29,7 @@ while($row = mysqli_fetch_assoc($result))
   $temparray = [];
   $sensor = $row["SensorID"];
   $q2 = "SELECT * FROM SensorType NATURAL JOIN SensorInstance WHERE SensorInstance.SensorID=$sensor";
+  error_log($q2, 0);
   $result2 = mysqli_query($conn, $q) or die("Error in Selecting " . mysqli_error($conn));
   while($row2 = mysqli_fetch_assoc($result2))
   {
