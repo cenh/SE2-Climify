d187626beab14b7c94e2daad25be662f4b9bd504
98christianp <christianpetersen98@gmail.com>
2018-11-24 15:00:54 +0100

    api get actions

---
+++ skoleklima/api/api-get-actions.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "../meta.php";
+
+if( $currentUserID == ""){
+    echo '{"status":"No UserID"}';
+    exit;
+}
+
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASSWORD;
+$databasename = DB_NAME;
+
+$conn = new mysqli($servername, $username, $password, $databasename);
+if($conn->connect_error) {
+    echo '{"status":"Connection died"}';
+    die("Connection error: " . $conn->connect_error);
+    exit;
+}
+
+$q = "SELECT * FROM Actions ";
+$result = mysqli_query($conn, $q) or die("Error in Selecting " . mysqli_error($conn));
+
+$emparray = array();
+while($row = mysqli_fetch_assoc($result))
+{
+    $temparray = [];
+    array_push($temparray[ActionID] = $row["ID"]);
+    array_push($temparray[Action] = $row["Description"]);
+
+    array_push($emparray, $temparray);
+}
+echo(json_encode($emparray));
+
+$conn->close();
+?>
