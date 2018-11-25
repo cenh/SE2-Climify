f023b82d10a571d7dd1621c8355a4d0763e0e78e
ciokos <wciokos@gmail.com>
2018-11-25 11:02:08 +0100

    devices list 160

--- skoleklima/api/api-get-last-sensors-data.php
+++ skoleklima/api/api-get-last-sensors-data.php
@@ -1,22 +1,8 @@
 <?php
 $body = $_POST["sensor"];
-switch ($body) {
-    case "readOutdoorTemperature":
-        $dbVariable = "Temperature";
-        break;
-    case "setTemperature":
-        $dbVariable = "Temperature";
-        break;
-    case "readHumidity":
-        $dbVariable = "Humidity";
-        break;
-    case "readBattery":
-        $dbVariable = "Battery";
-        break;
-}
 
 // TODO: any more things need to be called from this api?? (Hasn't been tested)
-$data = file_get_contents(   "http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20*%20FROM%20".$body);
+$data = file_get_contents(   "http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20LAST(*)%20FROM%20".$body);
 
 echo $data;
 
