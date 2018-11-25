a49fad0c07dabb42f041cffe4164337db5d9fd8c
s146498 <s146498@student.dtu.dk>
2018-11-25 15:07:21 +0100

    Update dashboard

--- skoleklima/ink/views/view-dashboard.php
+++ skoleklima/ink/views/view-dashboard.php
@@ -9,12 +9,14 @@
 			require_once "view-own-user.php";
 			require_once "view-system-settings.php";
 			require_once "view-roomX.php";
+			require_once "view-control-map.php";
 			require_once "view-rules.php";
 		} elseif ($currentUserRole == 15) {
 			require_once "view-data.php";
 			require_once "view-data-map.php";
 			require_once "view-own-user.php";
-            require_once "view-roomX.php";
+      require_once "view-roomX.php";
+			require_once "view-control-map.php";
 		} elseif ($currentUserRole == 2) {
 			require_once "view-devices.php";
 			require_once "view-data.php";
@@ -22,18 +24,21 @@
 			require_once "view-other-users.php";
 			require_once "view-own-user.php";
 			require_once "view-permissions.php";
-            require_once "view-roomX.php";
-            require_once "view-rules.php";
-        } elseif ($currentUserRole == 3) {
+      require_once "view-roomX.php";
+      require_once "view-rules.php";
+			require_once "view-control-map.php";
+    } elseif ($currentUserRole == 3) {
 			require_once "view-data.php";
 			require_once "view-data-map.php";
 			require_once "view-other-users.php";
 			require_once "view-own-user.php";
-            require_once "view-roomX.php";
+      require_once "view-roomX.php";
+			require_once "view-control-map.php";
 		} elseif ($currentUserRole == 4) {
 			require_once "view-data.php";
 			require_once "view-data-map.php";
-            require_once "view-roomX.php";
+      require_once "view-roomX.php";
+			require_once "view-control-map.php";
 		}
 		if($currentPermLogbook == 1 ){
 			require_once "view-communication.php";
@@ -47,7 +52,7 @@
 			<div>
 				<img src="img/logo/cook_ico.png" alt="Cookie">
 				<p>This site uses cookies to remember your settings.</br>
-				By continuing on the site, you accept the use of cookies. 
+				By continuing on the site, you accept the use of cookies.
 				</p>
 		</div>
 			<button id="btn-accept-cookie">OK</button>
