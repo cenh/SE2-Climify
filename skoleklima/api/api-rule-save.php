9c6c55c9718d738a36d75af94c00fbc722bb6deb
98christianp <christianpetersen98@gmail.com>
2018-11-25 12:39:55 +0100

    fixed api

--- skoleklima/api/api-rule-save.php
+++ skoleklima/api/api-rule-save.php
@@ -25,7 +25,7 @@ if ($conn->connect_error) {
   exit;
 }
 
-$q = "INSERT INTO Rule(SensorID, Operator, Value, Action) VALUES ('$sensorID','$Operator',$Value,'$Action')";
+$q = "INSERT INTO Rule(SensorID, Operator, Value, Action,ActuatorID) VALUES ('$sensorID','$Operator',$Value,'$Action','$ActuatorID')";
 $result = mysqli_query($conn, $q) or die("1Error in Selecting " . mysqli_error($conn));
 
 # Get the RuleID, so we can insert into RuleLocation
