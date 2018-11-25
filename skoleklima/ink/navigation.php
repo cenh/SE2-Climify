1ac52780d47b4305d4611f521633b8046c8ae9c0
s146498 <s146498@student.dtu.dk>
2018-11-25 14:52:05 +0100

    control-map

--- skoleklima/ink/navigation.php
+++ skoleklima/ink/navigation.php
@@ -100,8 +100,17 @@
 				<?php }
 				if(in_array("roomX", $perms))
 				{ ?>
-					<li class="menu-link menu-link-system-settings" data-go-to="roomX"></i>
+					<li class="menu-link menu-link-roomX" data-go-to="roomX"></i>
 							<a href="#/roomX">
+									<i class="menu-link-ico nav-icon fa fa-times-circle" aria-hidden="true"></i>
+									<p class="menu-link-text">Room X</p>
+							</a>
+					</li>
+				<?php }
+				if(in_array("control-map", $perms))
+				{ ?>
+					<li class="menu-link menu-link-control-map" data-go-to="control-map"></i>
+							<a href="#/control-map">
 									<i class="menu-link-ico nav-icon fa fa-times-circle" aria-hidden="true"></i>
 									<p class="menu-link-text">Room X</p>
 							</a>
