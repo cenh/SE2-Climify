41c25cd717a946f2d04d01a07ec609be1d6b7902
s146498 <s146498@student.dtu.dk>
2018-11-25 15:01:15 +0100

    Update icon

--- skoleklima/ink/navigation.php
+++ skoleklima/ink/navigation.php
@@ -111,8 +111,8 @@
 				{ ?>
 					<li class="menu-link menu-link-control-map" data-go-to="control-map"></i>
 							<a href="#/control-map">
-									<i class="menu-link-ico nav-icon fa fa-times-circle" aria-hidden="true"></i>
-									<p class="menu-link-text">Room X</p>
+									<i class="menu-link-ico nav-icon fa fa-sitemap" aria-hidden="true"></i>
+									<p class="menu-link-text">Control Map</p>
 							</a>
 					</li>
 				<?php }
