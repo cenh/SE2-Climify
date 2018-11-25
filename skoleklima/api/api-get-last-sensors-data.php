59d54d5ac39191fd80c03967bae9e2d782e3b1e4
ciokos <wciokos@gmail.com>
2018-11-25 11:34:08 +0100

    devices list 168

--- skoleklima/api/api-get-last-sensors-data.php
+++ skoleklima/api/api-get-last-sensors-data.php
@@ -5,11 +5,11 @@ $names = array_unique(json_decode($_POST['sensors_names']));
 $results = array();
 
 foreach ($names as $name) {
-    $data = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20LAST(*)%20FROM%20" . $name);
-    array_push($results, $data);
+//    $data = file_get_contents("http://localhost:8086/query?u=admin&p=groupc&db=scadb&q=SELECT%20LAST(*)%20FROM%20" . $name);
+    array_push($results, $name);
 }
 
-$resultsJSON = json_encode( $names , JSON_UNESCAPED_UNICODE );
+$resultsJSON = json_encode( $results , JSON_UNESCAPED_UNICODE );
 
 echo $resultsJSON;
 
