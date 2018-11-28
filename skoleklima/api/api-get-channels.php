d48e35292b5611b8135022f93a6e9879842af71a
christyyao <yao.christy@gmail.com>
2018-11-28 19:29:37 +0100

    control map get channels everyhting

--- skoleklima/api/api-get-channels.php
+++ skoleklima/api/api-get-channels.php
@@ -14,7 +14,8 @@ if ($conn->connect_error) {
     die("Connection error: " . $conn->connect_error);
 }
 
-$query = "SELECT * FROM Channels JOIN ThingsChannels WHERE ThingUID = $thingID";
+$query = "SELECT * FROM Channels";
+//JOIN ThingsChannels WHERE ThingUID = $thingID";
 
 $stmt = $conn->prepare($query);
 
