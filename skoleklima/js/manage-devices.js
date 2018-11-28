4bed3d59c98f036f19ca584c83083a8249b1faff
christyyao <yao.christy@gmail.com>
2018-11-28 19:19:27 +0100

    control map things delete options

--- skoleklima/js/manage-devices.js
+++ skoleklima/js/manage-devices.js
@@ -9,8 +9,9 @@ function refreshDevicesDropdown(roomID) {
     }, function (data) {
         var jData = JSON.parse(data);
         console.table(jData);
+        var x = document.getElementById("select_thing_manage_devices");
+        x.options.length = 0;
         for (var i = 0; i < jData.length; i++) {
-            var x = document.getElementById("select_thing_manage_devices");
             var option1 = document.createElement("option");
             option1.text = jData[i].Label;
             option1.value = jData[i].UID;
