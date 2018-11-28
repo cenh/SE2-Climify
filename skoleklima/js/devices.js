61d3a93942b5a355b2beb9683bb0b46c088087ca
christyyao <yao.christy@gmail.com>
2018-11-28 18:54:55 +0100

    control map room dropdown

--- skoleklima/js/devices.js
+++ skoleklima/js/devices.js
@@ -201,14 +201,19 @@ $(document).ready(function () {
         for (var i = 0; i < jData.length; i++) {
             var x = document.getElementById("select_room");
             var x_devices = document.getElementById("select_room_devices");
+            var x_manage_devices = document.getElementById("select_room_manage_devices");
             var option1 = document.createElement("option");
             var option2 = document.createElement("option");
+            var option3 = document.createElement("option");
             option1.text = jData[i].LocationName;
             option1.value = jData[i].LocationID;
             option2.text = jData[i].LocationName;
             option2.value = jData[i].LocationID;
+            option3.text = jData[i].LocationName;
+            option3.value = jData[i].LocationID;
             x.add(option1);
             x_devices.add(option2);
+            x_manage_devices.add(option3);
         }
     });
 });
