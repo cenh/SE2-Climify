77b3b0d12f4d72072dcf82ecc060ac4e003f828a
christyyao <yao.christy@gmail.com>
2018-11-28 19:15:29 +0100

    control map things :D

--- skoleklima/js/manage-devices.js
+++ skoleklima/js/manage-devices.js
@@ -1,4 +1,7 @@
 
+var things;
+
+
 function refreshDevicesDropdown(roomID) {
     var sUrl = "api/api-get-things.php";
     $.post(sUrl, {
@@ -6,5 +9,12 @@ function refreshDevicesDropdown(roomID) {
     }, function (data) {
         var jData = JSON.parse(data);
         console.table(jData);
+        for (var i = 0; i < jData.length; i++) {
+            var x = document.getElementById("select_thing_manage_devices");
+            var option1 = document.createElement("option");
+            option1.text = jData[i].Label;
+            option1.value = jData[i].UID;
+            x.add(option1);
+        }
     });
 }
