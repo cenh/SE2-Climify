3872f76f90050946fee65916053b5a60a3900307
ciokos <wciokos@gmail.com>
2018-12-04 11:24:15 +0100

    role permissions step 7

--- skoleklima/admin-login/js/roles.js
+++ skoleklima/admin-login/js/roles.js
@@ -44,26 +44,30 @@ function getTableData() {
         sessionToken: sessionToken,
     }, function (data) {
         var jData = JSON.parse(data);
-        permissions = jData;
-        var tmp = [];
-        for(var i = 0; i < permissions.length; i++) {
-            tmp.push(permissions[i].RoleName);
-        }
-        roles = Array.from(new Set(tmp));
-        var table = $('#roles_table').DataTable();
-        table.clear();
-
-        for (var j = 0; j < roles.length; j++) {
-            table.row.add([roles[j]]).draw(false);
-        }
+        roles = jData;
+        console.table(roles);
+        // var tmp = [];
+        // for(var i = 0; i < permissions.length; i++) {
+        //     tmp.push(permissions[i].RoleName);
+        // }
+        // roles = Array.from(new Set(tmp));
+        // var table = $('#roles_table').DataTable();
+        // table.clear();
+        //
+        // for (var j = 0; j < roles.length; j++) {
+        //     table.row.add([roles[j]]).draw(false);
+        // }
     });
 }
 
 function format_roles(d) {
     // `d` is the original data object for the row
-    // var index = actuators.findIndex(function (row) {
-    //     return row.Name === d[1];
-    // });
+    // var rows = ""
+    // for(var i = 0; i < permissions.length; i++) {
+    //     if(permissions[i].RoleName === d[0]) {
+    //         rows +=
+    //     }
+    // }
 
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
         '<tr>' +
