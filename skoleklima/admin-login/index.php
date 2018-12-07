459ea6b954610c3d9a6545b37180147533557aeb
Christian-Petersen1 <Christian.Petersen1@ibm.com>
2018-12-07 16:30:35 +0100

    bootstrap breaking studff

--- skoleklima/admin-login/index.php
+++ skoleklima/admin-login/index.php
@@ -32,7 +32,6 @@
             require_once "ink/dashboard.php";
         }
     ?>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src='https://www.google.com/recaptcha/api.js'></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
