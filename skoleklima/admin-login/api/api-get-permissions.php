8774b39071a0aa80702a133d50039ee8d8826cab
ciokos <wciokos@gmail.com>
2018-12-07 11:11:00 +0100

    role permissions step 18

--- skoleklima/admin-login/api/api-get-permissions.php
+++ skoleklima/admin-login/api/api-get-permissions.php
@@ -36,7 +36,7 @@ if ($conn->connect_error) {
 
 //$query = "SELECT RoleName, PermDescription FROM Role r JOIN RolePermission rp ON r.RoleID = rp.RoleID
 //        JOIN Permission p ON p.PermID = rp.PermID";
-$query = "SELECT * FROM Permission p LEFT JOIN RolePermission rp ON p.PermID = rp.PermID WHERE rp.RoleID = $roleID";
+$query = "SELECT * FROM Permission LEFT JOIN RolePermission ON RolePermission.PermID = Permission.PermID AND RolePermission.RoleID = $roleID";
 //RolePermission;
 //Permission;
 
