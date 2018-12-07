0c97d6874df4b0866c4fa6c521efff7a60fe628f
kacper <kacper.zyla97@gmail.com>
2018-12-07 16:02:11 +0100

    moved api-role-save.php added api-update-role.php

--- skoleklima/admin-login/api/api-role-save.php
+++ skoleklima/admin-login/api/api-role-save.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../meta.php";
+require_once "../admin-meta.php";
 
 if($currentUserID == ""){
   echo '{"status":"No UserID"}';
---
+++ skoleklima/admin-login/api/api-update-role.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "../admin-meta.php";
+
+if($currentUserID == ""){
+  echo '{"status":"No UserID"}';
+	exit;
+}
+
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASSWORD;
+$databasename = DB_NAME;
+
+
+$roleid = $_POST['RoleID'];
+$permissions = $_POST['Permissions'];
+
+
+$conn = new mysqli($servername, $username, $password, $databasename);
+if ($conn->connect_error) {
+  echo '{"status":"Connection died"}';
+  die("Connection error: " . $conn->connect_error);
+  exit;
+}
+
+$q = "DELETE FROM RolePermission WHERE RoleID=$roleid";
+mysqli_query($conn, $q) or die("Error in Selecting " . mysqli_error($conn));
+
+
+for($i = 0; $i < count($permissions); $i++){
+    $q = "INSERT INTO RolePermission(RoleID, PermID, InstID) VALUES ($roleid, $permissions[$i], 1)";
+    mysqli_query($conn, $q) or die("Error in Inserting " . mysqli_error($conn));
+}
+
+$conn->close();
+?>
