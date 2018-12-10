c4e0cbec4460447a001120ffd2093fb6365d6f15
Christian-Petersen1 <Christian.Petersen1@ibm.com>
2018-12-10 14:11:35 +0100

    removed modal

--- skoleklima/admin-login/js/main-al.js
+++ skoleklima/admin-login/js/main-al.js
@@ -333,21 +333,6 @@ function updateCompanyList(search) {
 </div>\
 </div>\
 </div>\
-<div class="modal fade" id="roleDropdown"  tabindex="-1" role="dialog" aria-labelledby="roleDropdownTitle" aria-hidden="true">\
-<div class="modal-dialog modal-dialog-centered" role="document">\
-<div class="modal-content">\n<div class="modal-header">\
-<h5 class="modal-title" id="roleDropdownTitle"</h5>\
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-<span aria-hidden="true">&times;</span>\
-</button>\n</div>\n<div class="modal-body">\
-<form id="newUserRole"></form>\n</div>\
-<div class="modal-footer">\
-<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-<button type="button" id="SaveRole" class="btn btn-primary">Save changes</button>\
-</div>\
-</div>\
-</div>\
-</div>\
 </div>\
 ';
 
