a96d724728baff9f6bd3d9251e5d420611149d2a
s146498 <s146498@student.dtu.dk>
2018-12-12 13:06:33 +0100

    Permissions change

--- skoleklima/ink/views/view-dashboard.php
+++ skoleklima/ink/views/view-dashboard.php
@@ -1,40 +1,63 @@
 <!-- Dashboard -->
 <div class="view view-dashboard">
 	<?php
-		$perms = array_values($permissions);
-		if(in_array("devices", $perms)) {
-			require_once "view-devices.php";
-		}
-		if(in_array("data-map", $perms)) {
-			require_once "view-data-map.php";
-		}
-		if(in_array("data", $perms)) {
-			require_once "view-data.php";
-		}
-		if(in_array("rules", $perms)) {
-			require_once "view-rules.php";
-		}
-		if(in_array("communication", $perms)) {
-			require_once "view-communication.php";
-		}
-		if(in_array("other-users", $perms)) {
-			require_once "view-other-users.php";
-		}
-		if(in_array("permissions", $perms)) {
-			require_once "view-permissions.php";
-		}
-		if(in_array("own-users", $perms)) {
-			require_once "view-own-user.php";
-		}
-		if(in_array("system-settings", $perms)) {
-			require_once "view-system-settings.php";
-		}
-		if(in_array("roomX", $perms)) {
-			require_once "view-roomX.php";
-		}
-		if(in_array("control-map", $perms)) {
-			require_once "view-control-map.php";
-		}
+	<?php
+	require_once "view-control-map.php";
+	if ($currentUserRole == 1) {
+				require_once "view-devices.php";
+				require_once "view-data.php";
+				require_once "view-data-map.php";
+				require_once "view-other-users.php";
+				require_once "view-own-user.php";
+				require_once "view-system-settings.php";
+				require_once "view-roomX.php";
+				require_once "view-rules.php";
+				require_once "view-control-map.php";
+			} elseif ($currentUserRole == 15) {
+				require_once "view-devices.php";
+				require_once "view-data.php";
+				require_once "view-data-map.php";
+				require_once "view-other-users.php";
+				require_once "view-own-user.php";
+				require_once "view-system-settings.php";
+				require_once "view-roomX.php";
+				require_once "view-rules.php";
+				require_once "view-control-map.php";
+			} elseif ($currentUserRole == 2) {
+				require_once "view-devices.php";
+				require_once "view-data.php";
+				require_once "view-data-map.php";
+				require_once "view-other-users.php";
+				require_once "view-own-user.php";
+				require_once "view-system-settings.php";
+				require_once "view-roomX.php";
+				require_once "view-rules.php";
+				require_once "view-control-map.php";
+	    } elseif ($currentUserRole == 3) {
+				require_once "view-devices.php";
+				require_once "view-data.php";
+				require_once "view-data-map.php";
+				require_once "view-other-users.php";
+				require_once "view-own-user.php";
+				require_once "view-system-settings.php";
+				require_once "view-roomX.php";
+				require_once "view-rules.php";
+				require_once "view-control-map.php";
+			} elseif ($currentUserRole == 4) {
+				require_once "view-devices.php";
+				require_once "view-data.php";
+				require_once "view-data-map.php";
+				require_once "view-other-users.php";
+				require_once "view-own-user.php";
+				require_once "view-system-settings.php";
+				require_once "view-roomX.php";
+				require_once "view-rules.php";
+				require_once "view-control-map.php";
+			}
+			if($currentPermLogbook == 1 ){
+				require_once "view-communication.php";
+			}
+			?>
 	?>
 	<div class="go-to-top">
 		<a href="<?php echo $company_Website ?>#top-header" target="_self"><i class="ico-go-to-top fa fa-arrow-up link" aria-hidden="true"></i></a>
