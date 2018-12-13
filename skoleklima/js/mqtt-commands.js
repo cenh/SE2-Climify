c3a900d95af11c98309ebea92c2f1e329007e693
ciokos <wciokos@gmail.com>
2018-12-13 14:04:56 +0100

    timer

--- skoleklima/js/mqtt-commands.js
+++ skoleklima/js/mqtt-commands.js
@@ -96,56 +96,62 @@ function set_actuator_number () {
 
 
 function listen() {
-    var drop = document.getElementById("select_room_devices");
+    var drop = document.getElementById("select_room_devices_listen");
     var roomID = drop.options[drop.selectedIndex].value;
     var rp = "";
     var sUrl = "api/api-get-rpID.php";
-    $.post(sUrl, {
-        roomID: roomID,
-    }, function (data) {
-        var jData = JSON.parse(data);
-        rp = jData[0].UID;
+    // $.post(sUrl, {
+    //     roomID: roomID,
+    // }, function (data) {
+    //     var jData = JSON.parse(data);
+    //     rp = jData[0].UID;
+    //
+    //     // Create a client instance
+    //     client = new Paho.MQTT.Client("iot.eclipse.org", Number(443), "/wss");
+    //     client.startTrace();
+    //     // set callback handlers
+    //     client.onConnectionLost = onConnectionLost;
+    //     //client.onMessageArrived = onMessageArrived;
+    //     // connect the client
+    //     client.connect({
+    //         onSuccess: onConnect,
+    //         useSSL: true
+    //     });
+    //     // console.log("attempting to connect...");
+    //
+    //     // called when the client connects
+    //     function onConnect() {
+    //         // Once a connection has been made, make a subscription and send a message.
+    //         // console.log("onConnect");
+    //         //client.subscribe("testse2");
+    //         msg = {
+    //             binding: "ZWAVE"
+    //         };
+    //         msg_text = JSON.stringify(msg);
+    //         message = new Paho.MQTT.Message(msg_text);
+    //         message.destinationName = "deviceDiscovery/" + rp;
+    //         client.publish(message);
+    //     }
+    //
+    //     // called when the client loses its connection
+    //     function onConnectionLost(responseObject) {
+    //         if (responseObject.errorCode !== 0) {
+    //             // console.log("onConnectionLost:" + responseObject.errorMessage);
+    //         }
+    //     }
+    //
+    //     // called when a message arrives
+    //     function onMessageArrived(message) {
+    //         msg = JSON.parse(message.payloadString);
+    //         // console.log("MessageArrived\n" + "Message id: " + msg['id'] + " message text: " + msg['text']);
+    //     }
+    //
+    // });
 
-        // Create a client instance
-        client = new Paho.MQTT.Client("iot.eclipse.org", Number(443), "/wss");
-        client.startTrace();
-        // set callback handlers
-        client.onConnectionLost = onConnectionLost;
-        //client.onMessageArrived = onMessageArrived;
-        // connect the client
-        client.connect({
-            onSuccess: onConnect,
-            useSSL: true
-        });
-        // console.log("attempting to connect...");
-
-        // called when the client connects
-        function onConnect() {
-            // Once a connection has been made, make a subscription and send a message.
-            // console.log("onConnect");
-            //client.subscribe("testse2");
-            msg = {
-                binding: "ZWAVE"
-            };
-            msg_text = JSON.stringify(msg);
-            message = new Paho.MQTT.Message(msg_text);
-            message.destinationName = "deviceDiscovery/" + rp;
-            client.publish(message);
-        }
-
-        // called when the client loses its connection
-        function onConnectionLost(responseObject) {
-            if (responseObject.errorCode !== 0) {
-                // console.log("onConnectionLost:" + responseObject.errorMessage);
-            }
-        }
-
-        // called when a message arrives
-        function onMessageArrived(message) {
-            msg = JSON.parse(message.payloadString);
-            // console.log("MessageArrived\n" + "Message id: " + msg['id'] + " message text: " + msg['text']);
-        }
-
-    });
+    var i = 12;
+    setInterval( function() {
+        console.log(i);
+        i--;
+    }, 1000);
 
 }
