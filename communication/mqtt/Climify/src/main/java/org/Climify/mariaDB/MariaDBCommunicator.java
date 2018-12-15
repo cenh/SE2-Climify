f96b3fbbdc1cc6119e8a100152481ef42448f8fc
Niclas Heltoft <niclas@niclasheltoft.dk>
2018-12-15 15:05:51 +0100

    Fixed category query

--- communication/mqtt/Climify/src/main/java/org/Climify/mariaDB/MariaDBCommunicator.java
+++ communication/mqtt/Climify/src/main/java/org/Climify/mariaDB/MariaDBCommunicator.java
@@ -484,8 +484,8 @@ public class MariaDBCommunicator {
 			PreparedStatement ps = connection.prepareStatement(sql);
 			ps.setString(1, sensor);
 			ResultSet result = ps.executeQuery();
-			System.out.println("Category Query Result: " + result.getString("Category"));
-			category = result.getString("Category");
+			result.next();
+			category = result.getString(1);
 		}catch (SQLException e) {
 			e.printStackTrace();
 		}
