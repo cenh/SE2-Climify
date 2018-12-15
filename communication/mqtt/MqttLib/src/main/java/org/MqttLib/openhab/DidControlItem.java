5ac6b7339de14735ad4b416ec54b460b61497a92
Statsminizzle <overload@live.dk>
2018-12-15 13:46:28 +0100

    Discovering / Managing devices implemented (#41)
    * Working on handling removal/approval of devices
    
    * removed unused code & minor refactoring to use same code-style as rest of project.
    
    * Functionality to:
    - Approve/Remove Things
    - Link/Unlink & Remove Items
    - Starting discovery of new Z-wave devices
    - Sending & Retrieval of the 'Inbox'  (The discovered devices)
    
    * Functionality to remove controllers from Things, StartDiscovery working, Bugfixes to delete/approve things/items
    
    * Things is now removed correctly. Thing from inbox is now removed when it is approved.
    
    * Approve now uses Post instead of put and correct headers
    
    * Channels are now being sent with the Thing doing approval

---
+++ communication/mqtt/MqttLib/src/main/java/org/MqttLib/openhab/DidControlItem.java
@@ -0,0 +1,19 @@
+package org.MqttLib.openhab;
+
+import com.dslplatform.json.CompiledJson;
+import com.dslplatform.json.CompiledJson.Behavior;
+
+@CompiledJson(onUnknown = Behavior.DEFAULT)
+public class DidControlItem {
+	public ControlType controlType;
+	public String uid;
+	public String channelUID;
+	public Item item;
+	
+	public DidControlItem(ControlType controlType, String uid, String channelUID, Item item) {
+		this.controlType = controlType;
+		this.uid = uid;
+		this.channelUID = channelUID;
+		this.item = item;
+	}
+}
