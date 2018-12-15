0eb0420d2ce3c2325fc199f0321fd48fcc27a88b
kacper <kacper.zyla97@gmail.com>
2018-12-15 12:56:03 +0100

    debug prints the name of the sensor with the most recent time

--- communication/mqtt/Climify/src/main/java/org/Climify/influxDB/InfluxCommunicator.java
+++ communication/mqtt/Climify/src/main/java/org/Climify/influxDB/InfluxCommunicator.java
@@ -102,7 +102,7 @@ public class InfluxCommunicator {
 				System.out.println("No measurements for RPi no " + i);
 			}
 			else{
-
+				System.out.println("Sensor: " + s);
 				System.out.println("Max time for RPI no: " + i + " is " + time + " for sensor " + s);
 			}
 			times.put(RPis.get(i),time);
