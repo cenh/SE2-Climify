fc36864b4a6d95e68bfd977aca1de987986ca666
ciokos <wciokos@gmail.com>
2018-12-21 16:33:25 +0100

    create role api 10

--- skoleklima/admin-login/api/api-create-role.php
+++ skoleklima/admin-login/api/api-create-role.php
@@ -38,7 +38,7 @@ $stmt->execute();
 
 $result = $stmt->get_result();
 
-$id = mysqli_fetch_assoc($result)[0];
+$id = mysqli_fetch_array($result)[0];
 
 $messages = json_encode($id, JSON_UNESCAPED_UNICODE);
 echo $messages;
