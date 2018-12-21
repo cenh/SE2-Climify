b99daf142e7623c13f999decdb8f61c405f8e9a9
ciokos <wciokos@gmail.com>
2018-12-21 14:56:00 +0100

    add new role and refresh roles

--- skoleklima/admin-login/ink/dashboard/view-devices.php
+++ skoleklima/admin-login/ink/dashboard/view-devices.php
@@ -45,7 +45,8 @@
     <div style="width: 100%; text-align: center;
          font-weight: bold; font-size: 150%;"> Roles
     </div>
-
+    <button>Add New Role</button>
+    <button>Refresh</button>
     <div style="width: 100%; height: auto; border: 1px solid #dddddd;
     text-align: left;
     padding: 8px; float:left;">
